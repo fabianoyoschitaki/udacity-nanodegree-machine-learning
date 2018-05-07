d58fa33ad847c1a36c9bf91825a1423edc544ae0
Fabiano Yoschitaki <fabianoyoschitaki@gmail.com>
2018-05-07 12:10:12 -0300

    feature scaling

--- ud120-projects/k_means/k_means_cluster.py
+++ ud120-projects/k_means/k_means_cluster.py
@@ -3,6 +3,7 @@
 """ 
     Skeleton code for k-means clustering mini-project.
 """
+
 import pickle
 import numpy
 import matplotlib.pyplot as plt
@@ -12,8 +13,7 @@ from feature_format import featureFormat, targetFeatureSplit
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
     """ some plotting code designed to help you visualize your clusters """
-
-    ### plot each cluster with a different color--add more colors for
+	### plot each cluster with a different color--add more colors for
     ### drawing more than five clusters
     colors = ["b", "c", "k", "m", "g"]
     for ii, pp in enumerate(pred):
@@ -29,14 +29,12 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.savefig(name)
     plt.show()
 
-
-
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
-#Stock Option Range#
+### Quiz 6 - Stock Option Range
 import pandas as pd
 
 df = pd.DataFrame(data_dict)
@@ -58,13 +56,12 @@ features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
-
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
 for f1, f2, _ in finance_features:
-    plt.scatter( f1, f2 )
+    plt.scatter( f1, f2)
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
